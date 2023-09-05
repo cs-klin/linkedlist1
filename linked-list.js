90696a1fb436a2b30d614a803f1672a3f24a81c3
Kai Lin <kevin_lin@alumni.brown.edu>
2023-09-05 19:28:31 -0400

    first

--- linked-list.js
+++ linked-list.js
@@ -7,12 +7,33 @@ class LinkedListNode {
 
 class LinkedList {
   constructor() {
+    this.length = 0;
+    this.head = null;
     // Your code here
   }
 
   addToHead(val) {
     // Your code here
+    const newNode = new LinkedListNode(val, null);
+
+    newNode.next = this.head;
+
+    this.head = newNode;
+
+    this.length += 1;
   }
+
+  // addToHead(value) {
+
+  //   // Create a new node with the given value
+  //   const newNode = new LinkedListNode(value, null);
+
+  //   // Set the node's `next` pointer to the list's current head pointer
+  //   newNode.next = this.head;
+
+  //   // Set the list head to point to the new link
+  //   this.head = newNode;
+  // }
 
   addToTail(val) {
     // Your code here
