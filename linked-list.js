930097bf20ef1e717bb7955068973cdc87388821
csauce <csacue295@gmail.com>
2023-09-05 18:53:25 -0500

    phease 1 done

--- linked-list.js
+++ linked-list.js
@@ -14,7 +14,7 @@ class LinkedList {
 
   addToHead(val) {
     // Your code here
-    const newNode = new LinkedListNode(val, null);
+    const newNode = new LinkedListNode(val);
 
     newNode.next = this.head;
 
@@ -23,20 +23,20 @@ class LinkedList {
     this.length += 1;
   }
 
-  // addToHead(value) {
-
-  //   // Create a new node with the given value
-  //   const newNode = new LinkedListNode(value, null);
-
-  //   // Set the node's `next` pointer to the list's current head pointer
-  //   newNode.next = this.head;
-
-  //   // Set the list head to point to the new link
-  //   this.head = newNode;
-  // }
 
   addToTail(val) {
-    // Your code here
+    const newNode = new LinkedListNode(val);
+
+    if( this.head === null) this.head = newNode
+
+    else {
+      let curr = this.head;
+      while(curr.next) {
+        curr = curr.next;
+      }
+      curr.next = newNode
+    }
+    this.length++
   }
 
   // You can use this function to help debug
